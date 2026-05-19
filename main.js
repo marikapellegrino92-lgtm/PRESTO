cb3fd3d31a1b7cd444791a6e439c5c0de644351a
Marika <marikapellegrino-igtm>
2026-05-19 17:05:30 +0200

    navbar terminata

--- main.js
+++ main.js
@@ -1,36 +1,26 @@
-let navbar = document.querySelector('.navbar');
+let navbar = document.querySelector('#navbar');
 let links = document.querySelectorAll('.nav-link');
-let logonavbar = document.querySelector('logoyellow');
+let logonavbar = document.querySelector('#logonavbar');
+console.dir(logonavbar);
 
-console.log(logoyellow);
-
-
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', ()=>{
     let scrolled = window.scrollY;
+   if(scrolled > 0){
+    navbar.classList.remove('bg-black');
+    navbar.classList.add('bg-yellow');
+    navbar.style.height= '70px';
+    links.forEach( (link)=>{
+     link.style.color = 'var(--black)';
+    });
+    logonavbar.src = 'http://127.0.0.1:5500/mediap/black.png'
+   }else{
+     navbar.classList.add('bg-black');
+    navbar.classList.remove('bg-yellow');
+    navbar.style.height= '140px';
+    links.forEach( (link)=>{
+     link.style.color = 'var(--yellow)';
+    });
+    logonavbar.src = 'http://127.0.0.1:5500/mediap/logoyellow.png'
+   } 
 
-   
-
-    if (scrolled > 0) {
-        navbar.classList.remove('bg-black');
-        navbar.classList.add('bg-yellow');
-        navbar.style.height = '70px';
-        links.forEach((link) => {
-         link.style.color = 'var(--black)';   
-        });
-       logoyellow.src = 'http://127.0.0.1:5500/mediap/black.png'
-    }else{
-        navbar.classList.add('bg-black');
-        navbar.classList.remove('bg-yellow');
-        navbar.style.height = '140px';
-         links.forEach((link) => {
-         link.style.color = 'var(--yellow)';   
-        });
-        logoyellow.src = 'http://127.0.0.1:5500/mediap/logoyellow.png'
-    }
 });
-
-
-  
-
-
-
